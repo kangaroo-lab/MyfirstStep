8fc28cbfc924e879204889f19e9296d56a74f33d
kangaroo-lab <kobano@icloud.com>
2022-09-15 15:05:34 +0900

    タイトルなど変動化

--- public/component/head.js
+++ public/component/head.js
@@ -3,7 +3,7 @@ document.write(`
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<metahttp-equiv="content-type" content="text/html; charset=UTF-8">
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="echo $description">
